1319ac852e61387f309374169e7df4ced173ae79
Chase Wright <mysticryuujin@protonmail.com>
2020-08-09 16:07:10 -0500

    Only return blockNumber
    Instead of returning the whole response data, only return the block number

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.get('/heartbeat', (req, res) => {
     axios.post(`https://api.archivenode.io/${process.env.API_KEY}`, data)
     .then((response) => {
         if('result' in response.data) {
-            res.status(200).json(response.data)
+            res.status(200).json(response.data.result)
             return
         } else {
             res.status(503).json({ error: response.data })
